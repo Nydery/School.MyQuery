d5e4589edd237ac71160af5f0bb3c92c1b99720d
Simon RS <simon.rauschschott@gmail.com>
2021-10-04 13:29:49 +0200

    added new extension methods

--- MyQuery.Logic/IEnumerableExtensions.cs
+++ MyQuery.Logic/IEnumerableExtensions.cs
@@ -18,6 +18,38 @@ namespace MyQuery.Logic
             return result;
         }
 
-        public static 
+        public static IEnumerable<TResult> Map<T, TResult> (this IEnumerable<T> source, Func<T, TResult> selector)
+        {
+            List<TResult> result = new List<TResult>();
+
+            foreach(T item in source)
+            {
+                result.Add(selector(item));
+            }
+
+            return result;
+        }
+
+        public static T[] ToArray<T> (this IEnumerable<T> source)
+        {
+            return new List<T>(source).ToArray();
+        }
+
+        public static List<T> ToList<T> (this IEnumerable<T> source)
+        {
+            return new List<T>(source);
+        }
+
+        public static double Sum<T> (this IEnumerable<T> source, Func<T, double> transform)
+        {
+            var result = 0.0;
+
+            foreach(T item in source)
+            {
+                result += transform(item);
+            }
+
+            return result;
+        }
     }
 }

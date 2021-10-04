392ffdf99a52508f728228ed16b0e9c31c900adf
Simon RS <simon.rauschschott@gmail.com>
2021-10-04 13:17:57 +0200

    Add project files.

---
+++ MyQuery.Logic/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyQuery.Logic
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Filter<T> (this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+
+            foreach(T item in source)
+            {
+                if (predicate(item))
+                    result.Add(item);
+            }
+
+            return result;
+        }
+
+        public static 
+    }
+}
